ca5f54afcc888f698a7777997ab7c5849ca97d24
dpoltura <dpoltura@2A4.42angouleme.fr>
2024-05-02 12:00:45 +0200

    up

--- philo/thread.c
+++ philo/thread.c
@@ -6,7 +6,7 @@
 /*   By: dpoltura <dpoltura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 16:13:30 by dpoltura          #+#    #+#             */
-/*   Updated: 2024/05/02 11:56:59 by dpoltura         ###   ########.fr       */
+/*   Updated: 2024/05/02 12:00:07 by dpoltura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,6 @@ static int	philo_is_die(t_philos *tmp)
 		pthread_mutex_unlock(&tmp->prev->r_fork_mutex);
 		pthread_mutex_unlock(&tmp->r_fork_mutex);
 		pthread_mutex_unlock(&tmp->next->l_fork_mutex);
-		if (tmp->table->end == 1)
-			return (0);
 		pthread_mutex_lock(&tmp->table->print_mutex);
 		printf("philo %d "ANSI_BOLDRED_HI"died\n"ANSI_RESET, tmp->philo_nb);
 		pthread_mutex_unlock(&tmp->table->print_mutex);
@@ -45,11 +43,6 @@ static int	philo_is_die(t_philos *tmp)
 }
 static void	philo_is_eating(t_philos *tmp)
 {
-	if (!philo_is_die(tmp) || tmp->table->end == 1)
-	{
-		pthread_detach(tmp->thread);
-		return ;
-	}
 	pthread_mutex_lock(&tmp->l_fork_mutex);
 	pthread_mutex_lock(&tmp->prev->r_fork_mutex);
 	pthread_mutex_lock(&tmp->r_fork_mutex);
@@ -68,11 +61,6 @@ static void	philo_is_eating(t_philos *tmp)
 
 static void	philo_is_sleeping(t_philos *tmp)
 {
-	if (!philo_is_die(tmp) || tmp->table->end == 1)
-	{
-		pthread_detach(tmp->thread);
-		return ;
-	}
 	pthread_mutex_lock(&tmp->table->print_mutex);
 	printf("philo %d is "ANSI_BOLDYELLOW_HI"sleeping\n"ANSI_RESET, tmp->philo_nb);
 	pthread_mutex_unlock(&tmp->table->print_mutex);
@@ -83,11 +71,6 @@ static void	philo_is_sleeping(t_philos *tmp)
 
 static void	philo_is_thinking(t_philos *tmp)
 {
-	if (!philo_is_die(tmp) || tmp->table->end == 1)
-	{
-		pthread_detach(tmp->thread);
-		return ;
-	}
 	pthread_mutex_lock(&tmp->table->print_mutex);
 	printf("philo %d is "ANSI_BOLDBLUE_HI"thinking\n"ANSI_RESET, tmp->philo_nb);
 	pthread_mutex_unlock(&tmp->table->print_mutex);
@@ -99,22 +82,13 @@ static void	*routine(void *cursor)
 
 	tmp = cursor;
 	if (!philo_is_die(tmp) || tmp->table->end == 1)
-	{
-		pthread_detach(tmp->thread);
 		return (NULL);
-	}
 	philo_is_eating(tmp);
-	if (!philo_is_die(tmp) || tmp->table->end == 1)
-	{
-		pthread_detach(tmp->thread);
+	if (!philo_is_die(tmp))
 		return (NULL);
-	}
 	philo_is_sleeping(tmp);
-	if (!philo_is_die(tmp) || tmp->table->end == 1)
-	{
-		pthread_detach(tmp->thread);
+	if (!philo_is_die(tmp))
 		return (NULL);
-	}
 	philo_is_thinking(tmp);
 	return (NULL);
 }
@@ -133,11 +107,6 @@ void	create_thread(t_table *table)
 			pthread_create(&cursor->thread, NULL, &routine, cursor);
 			cursor = cursor->next;
 			i++;
-		}
-		if (table->end == 1)
-		{
-			pthread_detach(cursor->thread);
-			return ;
 		}
 		cursor = table->philos;
 		i = 0;
