3df22ff39c44a72f1bb9216d309f25cc7dde51f5
dpoltura <dpoltura@1B4.42angouleme.fr>
2024-06-18 13:37:01 +0200

    up

--- cub3d/init_texture.c
+++ cub3d/init_texture.c
@@ -6,13 +6,13 @@
 /*   By: dpoltura <dpoltura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 11:14:54 by dpoltura          #+#    #+#             */
-/*   Updated: 2024/06/18 13:13:48 by dpoltura         ###   ########.fr       */
+/*   Updated: 2024/06/18 13:35:36 by dpoltura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void	is_ea_texture(char *str, int i, t_data_map *data_map)
+static int	is_ea_texture(char *str, int i, t_data_map *data_map)
 {
 	if (str[i] == 'E')
 	{
@@ -22,11 +22,15 @@ static void	is_ea_texture(char *str, int i, t_data_map *data_map)
 		if (str[i] == '.' && str[i + 1] == '/')
 			data_map->ea_texture = ft_strdup(&str[i]);
 		else
+		{
 			ft_error("Can't read texture\n");
+			return (0);
+		}
 	}
+	return (1);
 }
 
-static void	is_we_texture(char *str, int i, t_data_map *data_map)
+static int	is_we_texture(char *str, int i, t_data_map *data_map)
 {
 	if (str[i] == 'W')
 	{
@@ -36,11 +40,15 @@ static void	is_we_texture(char *str, int i, t_data_map *data_map)
 		if (str[i] == '.' && str[i + 1] == '/')
 			data_map->we_texture = ft_strdup(&str[i]);
 		else
+		{
 			ft_error("Can't read texture\n");
+			return (0);
+		}
 	}
+	return (1);
 }
 
-static void	is_so_texture(char *str, int i, t_data_map *data_map)
+static int	is_so_texture(char *str, int i, t_data_map *data_map)
 {
 	if (str[i] == 'S')
 	{
@@ -50,11 +58,15 @@ static void	is_so_texture(char *str, int i, t_data_map *data_map)
 		if (str[i] == '.' && str[i + 1] == '/')
 			data_map->so_texture = ft_strdup(&str[i]);
 		else
+		{
 			ft_error("Can't read texture\n");
+			return (0);
+		}
 	}
+	return (1);
 }
 
-static void	is_no_texture(char *str, int i, t_data_map *data_map)
+static int	is_no_texture(char *str, int i, t_data_map *data_map)
 {
 	if (str[i] == 'N')
 	{
@@ -64,8 +76,12 @@ static void	is_no_texture(char *str, int i, t_data_map *data_map)
 		if (str[i] == '.' && str[i + 1] == '/')
 			data_map->no_texture = ft_strdup(&str[i]);
 		else
+		{
 			ft_error("Can't read texture\n");
+			return (0);
+		}
 	}
+	return (1);
 }
 
 void	init_texture(char *str, t_data_map *data_map)
@@ -73,6 +89,10 @@ void	init_texture(char *str, t_data_map *data_map)
 	int	i;
 
 	i = 0;
+	data_map->no_texture = NULL;
+	data_map->so_texture = NULL;
+	data_map->we_texture = NULL;
+	data_map->ea_texture = NULL;
 	while (data_map->line)
 	{
 		free(data_map->line);
@@ -89,14 +109,14 @@ void	init_texture(char *str, t_data_map *data_map)
 		}
 		while (data_map->line[i] == ' ')
 			i++;
-		if (!data_map->no_texture)
-			is_no_texture(data_map->line, i, data_map);
-		if (!data_map->so_texture)
-			is_so_texture(data_map->line, i, data_map);
-		if (!data_map->we_texture)
-			is_we_texture(data_map->line, i, data_map);
-		if (!data_map->ea_texture)
-			is_ea_texture(data_map->line, i, data_map);
+		if (!is_no_texture(data_map->line, i, data_map))
+			return ;
+		if (!is_so_texture(data_map->line, i, data_map))
+			return ;
+		if (!is_we_texture(data_map->line, i, data_map))
+			return ;
+		if (!is_ea_texture(data_map->line, i, data_map))
+			return ;
 		free(data_map->line);
 		data_map->line = get_next_line(data_map->fd);
 		i = 0;
