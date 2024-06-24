51da4660f019a5cf54bd9ea20d57c8597de7e904
dpoltura <dpoltura@1A3.42angouleme.fr>
2024-06-24 12:44:44 +0200

    up

--- cub3d/main.c
+++ cub3d/main.c
@@ -6,7 +6,7 @@
 /*   By: dpoltura <dpoltura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 12:05:21 by dpoltura          #+#    #+#             */
-/*   Updated: 2024/06/24 11:42:10 by dpoltura         ###   ########.fr       */
+/*   Updated: 2024/06/24 12:29:30 by dpoltura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,6 +84,25 @@ int	main(int argc, char **argv)
 		}
 		free(data_map);
 		return (ft_error("Invalid map\n"));
+	}
+	if (!map_is_closed(data_map))
+	{
+		close(data_map->fd);
+		free(data_map->line);
+		free(data_map->no_texture);
+		free(data_map->so_texture);
+		free(data_map->we_texture);
+		free(data_map->ea_texture);
+		free(data_map->floor_color);
+		free(data_map->ceiling_color);
+		int i = 0;
+		while (data_map->map && data_map->map[i])
+		{
+			free(data_map->map[i]);
+			i++;
+		}
+		free(data_map);
+		return (ft_error("Map is not closed\n"));
 	}
 
 	int i = 0;
--- cub3d/parse_map.c
+++ cub3d/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: dpoltura <dpoltura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 14:14:27 by dpoltura          #+#    #+#             */
-/*   Updated: 2024/06/24 11:45:51 by dpoltura         ###   ########.fr       */
+/*   Updated: 2024/06/24 12:42:27 by dpoltura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,37 @@ static void	map_to_data(t_data_map *data_map)
 	}
 	data_map->map[j] = ft_strdup(data_map->line);
 	j++;
+}
+
+int	map_is_closed(t_data_map *data_map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (data_map->map[j])
+	{
+		if (!j || !data_map->map[j + 1])
+		{
+			while (data_map->map[j][i])
+			{
+				if (data_map->map[j][i] != '1' && data_map->map[j][i] != ' ' && data_map->map[j][i] != '\n')
+					return (0);
+				i++;
+			}
+		}
+		else
+		{
+			while (data_map->map[j][i])
+				i++;
+			if (data_map->map[j][i - 2] != '1')
+				return (0);
+		}
+		i = 0;
+		j++;
+	}
+	return (1);
 }
 
 int	map_exist(char *str, t_data_map *data_map)
@@ -54,7 +85,11 @@ int	map_exist(char *str, t_data_map *data_map)
 		}
 		while (data_map->line && data_map->line[i] == ' ')
 			i++;
-		if (data_map->line && data_map->line[i] == '1')
+		if (data_map->line && data_map->line[i] != 'N' && data_map->line[i] != 'S'
+			&& data_map->line[i] != 'W' && data_map->line[i] != 'E' && data_map->line[i] != '1'
+			&& data_map->line[i] != 'F' && data_map->line[i] != 'C' && data_map->line[i] != '\n')
+			return (0);
+		else if (data_map->line && data_map->line[i] == '1')
 			map_to_data(data_map);
 		else if (data_map->line && data_map->line[i] != '1' && data_map->map)
 			return (0);
